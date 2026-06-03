f924c889379ae2bea0a69ce36a4e6f66188bc935
Vaibhav Dubey <dubeyvaibhav1122@gmail.com>
2026-06-03 19:06:37 +0530

    days3

---
+++ Week_1/day_3/contmostwater.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left=0;
+        int right=height.size()-1;
+        int maxarea=0;
+        while(left<right){
+            int current_area=min(height[left],height[right])*(right-left);
+            maxarea=max(maxarea,current_area);
+            if (height[left]<height[right]){
+                left++;
+            }
+            else
+            right--;
+
+
+
+        }
+        
+        return maxarea;
+      
+    }
+};
---
+++ Week_1/day_3/movezeroes.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+                if (nums[i]==0){
+                    count+=1;
+                                  }
+
+        }
+     remove(nums.begin(),nums.end(),0);   
+     for(int i=0;i<count;i++){
+             nums.pop_back();
+
+        }
+     for(int i=0;i<count;i++){
+             nums.push_back(0);
+
+        }
+    }
+};
