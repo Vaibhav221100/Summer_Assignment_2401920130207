629a0eb165e284065c981e0dd4bab77c51fbd05d
Vaibhav Dubey <dubeyvaibhav1122@gmail.com>
2026-06-13 00:01:26 +0530

    Resolve merge conflict

--- Week_2/OOPS/music/wind/Saxophone.java
+++ Week_2/OOPS/music/wind/Saxophone.java
@@ -1,13 +1,11 @@
 package music.wind;
-<<<<<<< HEAD
+
 import music.Playable;
+
 public class Saxophone implements Playable {
+
     @Override
-=======
-public class Saxophone implements Playable {
-    @override
->>>>>>> 2bade6725bd89d238ad187deec0cac3edf483864
-    public void play(){
-        System.out.println(" playing Saxophone");
+    public void play() {
+        System.out.println("Playing Saxophone");
     }
 }
