eb41293f6b16e612950224a6b31a2a8af88c4fc0
Vaibhav Dubey <dubeyvaibhav1122@gmail.com>
2026-06-22 22:28:15 +0530

    w4d1

---
+++ Week_3/day_5/queueUsingStack.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyQueue {
+public:
+    stack<int> input,output;
+
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        input.push(x);
+    }
+    
+    int pop() {
+        int val=peek();
+        output.pop();
+        return val;
+    }
+    
+    int peek() {
+        if (output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        int val=output.top();
+        return val;
+    }
+    
+    bool empty() {
+        return (input.empty() && output.empty());
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
