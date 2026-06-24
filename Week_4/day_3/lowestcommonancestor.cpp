47ed14a1a7400d83b86f64996702e1d66ebf1921
Vaibhav Dubey <dubeyvaibhav1122@gmail.com>
2026-06-24 23:03:18 +0530

    w4d3

---
+++ Week_4/day_3/lowestcommonancestor.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val ;
+    TreeNode*left;
+    TreeNode*right;
+};
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root){
+            if(p->val > root->val && q->val > root->val) root=root->right;
+            else if (p->val < root->val && q->val < root->val) root=root->left;
+            else return root;
+        }
+        return nullptr;
+    }
+};
---
+++ Week_4/day_3/searchBST.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode*left;
+    TreeNode*right;    
+};
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root) return root;
+        if (root->val==val) return root;
+        if (root->val>val) return searchBST(root->left,val);
+         return searchBST(root->right,val);
+    }
+};
